460c9f17ecf8d6deb17c4fcf3d760eb654eef779
Ethan Zhang <ethan@paraflow.com>
2025-09-01 07:56:45 +0000

    Revert "fix: correct release-please output format for monorepo packages (#9)"
    This reverts commit 9a941a9b78654a2f9df8410506e2583c8a63ae96.

--- turbo/apps/web/drizzle.config.ts
+++ turbo/apps/web/drizzle.config.ts
@@ -1,17 +1,14 @@
 import { defineConfig } from "drizzle-kit";
+import { env } from "./src/env";
 
 export const DRIZZLE_MIGRATE_OUT = "./src/db/migrations";
-
-if (!process.env.DATABASE_URL) {
-  throw new Error("invalid DATABASE_URL env");
-}
 
 export default defineConfig({
   schema: "./src/db/schema/*",
   out: DRIZZLE_MIGRATE_OUT,
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DATABASE_URL,
+    url: env().DATABASE_URL,
   },
   verbose: true,
   strict: false,
