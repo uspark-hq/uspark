b6d004e3d4e2b364e03fb4eaa435950c7cfc446e
Ethan Zhang <ethan@uspark.ai>
2025-09-06 03:31:17 +0000

    chore: remove unused file-explorer exports from index

--- turbo/apps/web/app/components/file-explorer/index.ts
+++ turbo/apps/web/app/components/file-explorer/index.ts
@@ -1,17 +1,2 @@
 export { FileExplorer } from "./file-explorer";
 export { YjsFileExplorer } from "./yjs-file-explorer";
-export { FileIcon } from "./file-icon";
-export { FileTreeItem } from "./file-tree-item";
-export { buildFileTree } from "./utils";
-export {
-  parseYjsFileSystem,
-  formatFileSize,
-  formatModifiedTime,
-} from "./yjs-parser";
-export type {
-  FileItem,
-  FileTree,
-  FileIconProps,
-  FileExplorerProps,
-  FileTreeItemProps,
-} from "./types";
