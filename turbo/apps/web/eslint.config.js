5fd84d319120e86eb31d9e71ced6edab2102cc57
Ethan Zhang <ethan@uspark.ai>
2025-09-10 03:19:21 +0000

    refactor: convert web app eslint config to typescript

--- turbo/apps/web/eslint.config.js
+++
@@ -1,4 +0,0 @@
-import { nextJsConfig } from "@uspark/eslint-config/next-js";
-
-/** @type {import("eslint").Linter.Config} */
-export default nextJsConfig;
---
+++ turbo/apps/web/eslint.config.ts
@@ -0,0 +1,6 @@
+import { nextJsConfig } from "@uspark/eslint-config/next-js";
+import type { Linter } from "eslint";
+
+const config: Linter.Config[] = nextJsConfig;
+
+export default config;
